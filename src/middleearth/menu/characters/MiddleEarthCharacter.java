2188c607c4359d13385876e55477e79423511694
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-02-25 07:06:30 -0600

    Added MiddleEarthCharacter Class
    Added the abstract class MiddleEarthCharacter and created a
    parameterized constructor to initialize new Middle Earth characters.

---
+++ src/middleearth/menu/characters/MiddleEarthCharacter.java
@@ -0,0 +1,27 @@
+package middleearth.menu.characters;
+
+public abstract class MiddleEarthCharacter {
+	
+	String name;
+	Double health;
+	Double power;
+
+	/**
+	 * This is a parameterized constructor that allows the user to create
+	 * a Middle Earth Character with a name, health, and power.
+	 * @param name (String)
+	 * 		This argument is the name of the character.
+	 * @param helath (Double)
+	 * 		This argument is the health of the character.
+	 * @param power (Double)
+	 * 		This argument is the power of the character.
+	 * */
+	public MiddleEarthCharacter(String name, Double health, Double power) {
+		this.name = name;
+		this.health = health;
+		this.power = power;
+	}
+	
+	
+	
+}
