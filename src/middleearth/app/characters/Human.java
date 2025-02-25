facf469986a66a4781b928c137ff1d07117947bf
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-02-25 10:38:52 -0600

    Human attack() Method Completed
    Wrote the Human attack() method which specifies how an attack will
    affect different Middle Earth races. Javadoc comments included.

--- src/middleearth/app/characters/Human.java
+++ src/middleearth/app/characters/Human.java
@@ -6,9 +6,38 @@ public class Human extends MiddleEarthCharacter {
 		super(name, health, power);
 	}
 
+	/**
+	 * Performs an attack on a target based on the rules of a human
+	 * attack on the targets race.
+	 * @param target (MiddleEarthCharacter)
+	 * 		The argument is the character to be attacked.
+	 * */
 	@Override
 	boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
+		
+		// Humans attack Wizards w/ x1.5 power
+		if(target.getClass().getSimpleName().equals("Wizard"))
+		{
+			target.health -= 1.5 * power;
+			return true;
+		}
+		
+		// Humans attack Orcs and fellow Humans w/ no power
+		if(target.getClass().getSimpleName().equals("Orc") ||
+				target.getClass().getSimpleName().equals("Human"))
+		{
+			target.health -= 0;
+			return true;
+		}
+		
+		// Humans attack Elves and Dwarfs w/ normal power
+		if(target.getClass().getSimpleName().equals("Elf") ||
+				target.getClass().getSimpleName().equals("Dwarf"))
+		{
+			target.health -= power;
+			return true;
+		}
+			
 		return false;
 	}
 
