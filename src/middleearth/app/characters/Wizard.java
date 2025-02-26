b4ea40aef703ddeb7180c2bf4dc9a0f2542b174d
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-02-25 22:29:10 -0600

    Created a Wizard Method, but Not Finished
    Need to change the rules for wizard attack() method, but added the base.

--- src/middleearth/app/characters/Wizard.java
+++ src/middleearth/app/characters/Wizard.java
@@ -6,9 +6,40 @@ public class Wizard extends MiddleEarthCharacter {
 		super(name, health, power);
 	}
 
+	// GABI YOU DIDN'T FINISH THIS FUNCTION
+	
+	/**
+	 * Performs an attack on a target based on the rules of a wizard
+	 * attack on the targets race.
+	 * @param target (MiddleEarthCharacter)
+	 * 		The argument is the character to be attacked.
+	 * */
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
-		// TODO Auto-generated method stub
+		
+		// Orcs attack Humans w/ x1.5 power
+		if(target.getClass().getSimpleName().equals("Human"))
+		{
+			target.health -= 1.5 * power;
+			return true;
+		}
+		
+		// Orcs attack Elves and fellow Orcs w/ no power
+		if(target.getClass().getSimpleName().equals("Orc") ||
+				target.getClass().getSimpleName().equals("Elf"))
+		{
+			target.health -= 0;
+			return true;
+		}
+		
+		// Orcs attack Wizards and Dwarfs w/ normal power
+		if(target.getClass().getSimpleName().equals("Wizard") ||
+				target.getClass().getSimpleName().equals("Dwarf"))
+		{
+			target.health -= power;
+			return true;
+		}
+			
 		return false;
 	}
 
